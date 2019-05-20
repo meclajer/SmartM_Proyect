f1dffc3e021fcfcd509160f8b4f1d385e7fdc23e
meclajer <meclajer@gmail.com>
2019-05-20 02:08:36 -0500

    Panalla principal y anexo de calendario mas preparativo listvwiew

--- app/src/main/java/com/proyect/itsjsexto/smartm_proyect/pantallaPrincipal.java
+++ app/src/main/java/com/proyect/itsjsexto/smartm_proyect/pantallaPrincipal.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -15,20 +17,33 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class pantallaPrincipal extends AppCompatActivity
         implements OnMapReadyCallback {
 
+    //Mapa
     GoogleMap map;
+
+    //
+    ListView lista;
+    String[] valLista = new String[] {"Checar llantas","Alineacion","Amortiguadores"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.app_bar_pantalla_con_menu);
 
+        //Mapa
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
+        //Lista
+        lista = (ListView) findViewById(R.id.listviweChequeo);
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,valLista);
+
+        lista.setAdapter(adaptador);
+
     }
 
+    //Mapa
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map.addMarker(new MarkerOptions()
@@ -37,11 +52,13 @@ public class pantallaPrincipal extends AppCompatActivity
 
     }
 
+    //Mapa
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
         map.getMaxZoomLevel();
     }
 
+    //Mapa
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
