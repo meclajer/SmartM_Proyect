a06c030b48fc8d7a625740f1d0f102124ff976fc
fjoseab <47824580+fjoseab@users.noreply.github.com>
2019-05-27 13:27:36 -0500

    Modificacion base de datos

--- app/src/main/java/com/proyect/itsjsexto/smartm_proyect/DatosMoto.java
+++ app/src/main/java/com/proyect/itsjsexto/smartm_proyect/DatosMoto.java
@@ -6,10 +6,6 @@ import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 @Entity
 public class DatosMoto {
-    @PrimaryKey
-    @NonNull
-    //Debe ser foranea no se como
-    private String correo;
 
     @PrimaryKey
     @NonNull
@@ -28,4 +24,58 @@ public class DatosMoto {
     @NonNull
     private String temperatura;
 
+    public DatosMoto( @NonNull String numSer, @NonNull String latitud, @NonNull String longitud, @NonNull String presion, @NonNull String temperatura) {
+
+        this.numSer = numSer;
+        this.latitud = latitud;
+        this.longitud = longitud;
+        this.presion = presion;
+        this.temperatura = temperatura;
+    }
+
+
+    @NonNull
+    public String getNumSer() {
+        return numSer;
+    }
+
+    public void setNumSer(@NonNull String numSer) {
+        this.numSer = numSer;
+    }
+
+    @NonNull
+    public String getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(@NonNull String latitud) {
+        this.latitud = latitud;
+    }
+
+    @NonNull
+    public String getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(@NonNull String longitud) {
+        this.longitud = longitud;
+    }
+
+    @NonNull
+    public String getPresion() {
+        return presion;
+    }
+
+    public void setPresion(@NonNull String presion) {
+        this.presion = presion;
+    }
+
+    @NonNull
+    public String getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(@NonNull String temperatura) {
+        this.temperatura = temperatura;
+    }
 }
